4ec6982d8a44228fd997eddbf07e589c4ac1d6d8
Ashutosh <ashutoshtiwari3309@gmail.com>
2021-01-27 16:07:54 +0530

    Complete: @OneToOne | Using Spring | Uni-directional

--- hibernate-demo/src/com/inclined/jdbc/TestJdbc.java
+++ hibernate-demo/src/com/inclined/jdbc/TestJdbc.java
@@ -7,7 +7,7 @@ public class TestJdbc {
 
 	public static void main(String[] args) {
 
-		String jdbcURL = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false&serverTimezone=UTC";
+		String jdbcURL = "jdbc:mysql://localhost:3306/hb-01-one-to-one-uni?useSSL=false&serverTimezone=UTC";
 		String username = "hbstudent";
 		String password = "hbstudent";
 		
